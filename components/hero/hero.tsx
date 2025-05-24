c6eb4d7d4a5eb37d2c69e580cb5d1d34417ba943
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-24 17:03:23 +0300

    Update hero.tsx

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -1,4 +1,5 @@
-import { Container, Flex, FlexProps, Text, VStack } from '@chakra-ui/react'
+import { Container, Flex, FlexProps, Text, VStack, Box, Icon } from '@chakra-ui/react'
+import { FiArrowRight } from 'react-icons/fi'
 
 interface HeroProps extends Omit<FlexProps, 'title'> {
   title: string | React.ReactNode
@@ -7,23 +8,48 @@ interface HeroProps extends Omit<FlexProps, 'title'> {
 
 export const Hero = ({ title, description, children, ...rest }: HeroProps) => {
   return (
-    <Flex py="20" alignItems="center" {...rest}>
+    <Flex 
+      py={{ base: 12, md: 20 }} 
+      alignItems="center" 
+      {...rest}
+    >
       <Container>
         <VStack spacing={[4, null, 8]} alignItems="flex-start">
-          <Text as="h1" textStyle="h1" textAlign="left">
+          <Text 
+            as="h1" 
+            textStyle="h1" 
+            textAlign="left"
+            fontSize={{ base: "3xl", md: "5xl" }}
+            lineHeight="shorter"
+          >
             {title}
           </Text>
           <Text
             as="div"
             textStyle="subtitle"
             align="left"
-            color="gray.500"
-            _dark={{ color: 'gray.400' }}
+            color="black"
+            _dark={{ color: 'whiteAlpha.800' }}
+            fontSize={{ base: "md", md: "xl" }}
+            fontWeight="medium"
           >
             {description}
           </Text>
         </VStack>
-        {children}
+        <Box mt={8}>
+          {React.Children.map(children, child => {
+            if (React.isValidElement(child)) {
+              return React.cloneElement(child, {
+                // Add orange color to contact buttons
+                ...(child.props.href === "/contact" ? { 
+                  colorScheme: "orange",
+                  variant: "solid"
+                } : {})
+              } as any)
+            }
+            return child
+          })}
+        </Box>
       </Container>
     </Flex>
   )
