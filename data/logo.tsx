869b776799b619921c396661cfb19a689dcb89f4
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-24 20:24:05 +0300

    Update logo.tsx

--- data/logo.tsx
+++ data/logo.tsx
@@ -3,8 +3,7 @@ import { chakra, HTMLChakraProps, useColorModeValue } from '@chakra-ui/react'
 export const Logo: React.FC<HTMLChakraProps<'svg'>> = (props) => {
   const color = useColorModeValue('#231f20', '#fff')
   return (
-    <chakra.svg
-     
-    </chakra.svg>
+    <chakra.svg></chakra.svg>
+    
   )
 }
