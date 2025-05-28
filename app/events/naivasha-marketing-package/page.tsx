40c12bc01b4de182f69f9677412aefc96dbeb0b1
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-28 19:15:56 +0300

    Delete app/events directory

--- app/events/naivasha-marketing-package/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/events/naivasha-marketing-package/page.tsx
-
-"use client";
-
-import NaivashaMarketingPackage from "@/components/NaivashaMarketingPackage";
-
-export default function NaivashaMarketingPackagePage() {
-  return <NaivashaMarketingPackage />;
-}
