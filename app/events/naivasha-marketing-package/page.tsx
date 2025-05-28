9a95c686a22e61305013fa9aa42728b2e3f16757
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-28 19:01:45 +0300

    Create page.tsx

---
+++ app/events/naivasha-marketing-package/page.tsx
@@ -0,0 +1,9 @@
+// app/events/naivasha-marketing-package/page.tsx
+
+"use client";
+
+import NaivashaMarketingPackage from "@/components/NaivashaMarketingPackage";
+
+export default function NaivashaMarketingPackagePage() {
+  return <NaivashaMarketingPackage />;
+}
