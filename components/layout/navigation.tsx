5b5fc3961ebed4af8da09e47e9f6ce9807784106
Muriuki-dev <youjokealot@gmail.com>
2025-09-07 14:43:08 +0300

    Update navigation.tsx

--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -1,4 +1,4 @@
-import { HStack } from '@chakra-ui/react'
+import { HStack, Flex, useColorModeValue } from '@chakra-ui/react'
 import { useDisclosure, useUpdateEffect } from '@chakra-ui/react'
 import { useScrollSpy } from 'hooks/use-scrollspy'
 import { usePathname, useRouter } from 'next/navigation'
@@ -31,37 +31,49 @@ const Navigation: React.FC = () => {
     mobileNavBtnRef.current?.focus()
   }, [mobileNav.isOpen])
 
+  // Dynamic colors for dark/light mode
+  const activeColor = useColorModeValue('purple.600', 'purple.300')
+  const linkColor = useColorModeValue('gray.700', 'gray.200')
+
   return (
-    <HStack spacing="2" flexShrink={0}>
-      {siteConfig.header.links.map(({ href, id, ...props }, i) => {
-        return (
-          <NavLink
-            display={['none', null, 'block']}
-            href={href || `/#${id}`}
-            key={i}
-            isActive={
-              !!(
-                (id && activeId === id) ||
-                (href && !!path?.match(new RegExp(href)))
-              )
-            }
-            {...props}
-          >
-            {props.label}
-          </NavLink>
-        )
-      })}
+    <Flex
+      flex="1"
+      justify={{ base: 'flex-start', lg: 'center' }} // 👈 Center nav on lg+
+      align="center"
+    >
+      <HStack spacing="6" flexShrink={0}>
+        {siteConfig.header.links.map(({ href, id, ...props }, i) => {
+          const isActive =
+            (id && activeId === id) ||
+            (href && !!path?.match(new RegExp(href)))
 
-      <ThemeToggle />
+          return (
+            <NavLink
+              display={['none', null, 'block']}
+              href={href || `/#${id}`}
+              key={i}
+              color={isActive ? activeColor : linkColor} // 👈 dynamic color
+              fontWeight={isActive ? 'bold' : 'medium'}
+              {...props}
+            >
+              {props.label}
+            </NavLink>
+          )
+        })}
 
-      <MobileNavButton
-        ref={mobileNavBtnRef}
-        aria-label="Open Menu"
-        onClick={mobileNav.onOpen}
-      />
+        {/* Theme toggle adapts automatically */}
+        <ThemeToggle />
 
-      <MobileNavContent isOpen={mobileNav.isOpen} onClose={mobileNav.onClose} />
-    </HStack>
+        {/* Mobile nav only visible on small screens */}
+        <MobileNavButton
+          ref={mobileNavBtnRef}
+          aria-label="Open Menu"
+          onClick={mobileNav.onOpen}
+        />
+
+        <MobileNavContent isOpen={mobileNav.isOpen} onClose={mobileNav.onClose} />
+      </HStack>
+    </Flex>
   )
 }
 
